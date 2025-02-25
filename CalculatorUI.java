041f832b137a2cbd77fb16293f7720ec0a771c2e
aakriti <aakritip512@gmail.com>
2025-02-25 09:16:28 +0545

    inital commit

--- CalculatorUI.java
+++ CalculatorUI.java
@@ -1,5 +1,5 @@
 public class CalculatorUI{
     public static void main(String[] args) {
-        System.out.println("First commit");
+        System.out.println("First commit for java calculator project");
     }
 }
